e24b0806afb19803906fa1dbfe7794e36f4c1304
bcattaneo <c@ttaneo.uy>
2021-11-21 21:55:30 -0300

    cambios para API en node

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,42 +1,129 @@
-// Stores people that contacted me
-let contacts = [];
-let modal;
-
-const getModalInputs = () => document.querySelectorAll("#modal-form")[0];
-
-const closeModal = () => {
-    if (!modal) return;
-
+"use strict";
+var apiBaseUrl = "https://PW2021-APINode-bcattaneo-4.bcattaneo.repl.co"; // CHANGEME
+var endpointExperiencia = "/experiencia-laboral";
+var endpointFormulario = "/enviar-formulario";
+var modal;
+var errorAlert;
+var errorAlertMessage;
+var handleErrors = function (response) {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+};
+var postContact = function (contact) {
+    fetch("".concat(apiBaseUrl).concat(endpointFormulario), {
+        method: "POST",
+        mode: "cors",
+        headers: new Headers({ "content-type": "application/json" }),
+        body: JSON.stringify(contact),
+    })
+        .then(handleErrors)
+        .then(function (response) { return console.log("OK"); })
+        .catch(function (error) {
+        showError("Error al enviar formulario");
+        console.error("Error:", error);
+    });
+};
+var parseDate = function (dateString) {
+    return new Date(Date.parse(dateString));
+};
+var getExperienceYear = function (date) {
+    var dateFullYear = date.getFullYear();
+    return dateFullYear == new Date().getFullYear() ? "Present" : dateFullYear;
+};
+var getWorkExperience = function () {
+    fetch("".concat(apiBaseUrl).concat(endpointExperiencia), {
+        method: "GET",
+        mode: "cors",
+        headers: new Headers({ "content-type": "application/json" }),
+    })
+        .then(handleErrors)
+        .then(function (response) { return response.json(); })
+        .then(function (data) {
+        console.log(data["experiencia-laboral"]);
+        var workExperience = data["experiencia-laboral"];
+        var experienceItems = "";
+        var first = true;
+        for (var _b = 0, workExperience_1 = workExperience; _b < workExperience_1.length; _b++) {
+            var experience = workExperience_1[_b];
+            experienceItems += "<div class=\"".concat(first ? "" : "mt-4", " text-gray-600 w-full mt-2 overflow-hidden\">\n                <p class=\"text-black font-bold\">").concat(experience.puesto, "</p>\n                <p>").concat(experience.empresa, "</p>\n                <p class=\"italic\">").concat(parseDate(experience.fechaInicio).getFullYear(), "-").concat(getExperienceYear(parseDate(experience.fechaFin)), "</p>\n                <p class=\"mt-2\">").concat(experience.descripcion, "</p>\n                </div>");
+            first = false;
+        }
+        var experienceItemsElem = document.getElementById("experience-items");
+        if (experienceItemsElem !== null)
+            experienceItemsElem.innerHTML = experienceItems;
+    })
+        .catch(function (error) {
+        showError("Error al obtener experiencias");
+        console.error("Error:", error);
+    });
+};
+var getModalInputs = function () {
+    return document.querySelectorAll("#modal-form");
+};
+var closeModal = function () {
+    if (!modal)
+        return;
     // Clean inputs
-    for (let n of getModalInputs())
+    var nodeModalInputs = getModalInputs();
+    var modalInputs = nodeModalInputs[0];
+    for (var _i = 0, _a = modalInputs; _i < _a.length; _i++) {
+        var n = _a[_i];
         n.value = "";
-
+    }
     modal.classList.add("hidden");
-}
-
-const submitModal = () => {
+};
+var submitModal = function () {
     // Read and store inputs
-    let contact = {}
-    for (let n of getModalInputs())
+    var contact = {};
+    var nodeModalInputs = getModalInputs();
+    var modalInputs = nodeModalInputs[0];
+    for (var _i = 0, _a = modalInputs; _i < _a.length; _i++) {
+        var n = _a[_i];
         contact[n.id] = n.value;
-    contacts.push(contact);
-
-    console.log(contacts);
-
-    // TODO: validation on submit
+    }
+    console.log(contact);
+    postContact(contact);
     closeModal();
-}
-
-const showModal = () => modal.classList.remove("hidden");
-
-(function() {
-    // Locate and define global modal
-    modal = document.querySelector("#contact-modal") || (function() { throw "No modal" })();
-    console.log(modal);
-    console.log(contacts);
-
-    // Add listeners
-    document.querySelector("#modal-submit").addEventListener("click", submitModal);
-    document.querySelector("#modal-cancel").addEventListener("click", closeModal);
-    document.querySelector("#contact-me").addEventListener("click", showModal);
+};
+var showModal = function () {
+    modal.classList.remove("hidden");
+};
+var showError = function (text) {
+    errorAlertMessage.innerHTML = text;
+    errorAlert.classList.remove("hidden");
+};
+var closeError = function () {
+    errorAlert.classList.add("hidden");
+};
+(function () {
+    modal =
+        document.querySelector("#contact-modal") ||
+            (function () {
+                throw "No modal";
+            })();
+    errorAlert =
+        document.querySelector("#error-alert") ||
+            (function () {
+                throw "No error alert";
+            })();
+    errorAlertMessage =
+        document.querySelector("#error-alert-message") ||
+            (function () {
+                throw "No error alert message";
+            })();
+    var modalSubmit = document.querySelector("#modal-submit");
+    if (modalSubmit != null)
+        modalSubmit.addEventListener("click", submitModal);
+    var modalCancel = document.querySelector("#modal-cancel");
+    if (modalCancel != null)
+        modalCancel.addEventListener("click", closeModal);
+    var contactMe = document.querySelector("#contact-me");
+    if (contactMe != null)
+        contactMe.addEventListener("click", showModal);
+    var errorAlertClose = document.querySelector("#error-alert-close");
+    if (errorAlertClose != null)
+        errorAlertClose.addEventListener("click", closeError);
+    getWorkExperience();
 })();
---
+++ src/script.ts
@@ -0,0 +1,146 @@
+const apiBaseUrl = "https://PW2021-APINode-bcattaneo-4.bcattaneo.repl.co"; // CHANGEME
+const endpointExperiencia = "/experiencia-laboral";
+const endpointFormulario = "/enviar-formulario";
+
+let modal: HTMLElement;
+let errorAlert: HTMLElement;
+let errorAlertMessage: HTMLElement;
+
+var handleErrors = function (response: any) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+};
+
+var postContact = function (contact: any) {
+  fetch(`${apiBaseUrl}${endpointFormulario}`, {
+    method: "POST",
+    mode: "cors",
+    headers: new Headers({ "content-type": "application/json" }),
+    body: JSON.stringify(contact),
+  })
+    .then(handleErrors)
+    .then((response) => console.log("OK"))
+    .catch((error) => {
+      showError("Error al enviar formulario");
+      console.error("Error:", error);
+    });
+};
+var parseDate = function (dateString: string) {
+  return new Date(Date.parse(dateString));
+};
+
+var getExperienceYear = function (date: Date) {
+  const dateFullYear = date.getFullYear();
+  return dateFullYear == new Date().getFullYear() ? "Present" : dateFullYear;
+};
+
+var getWorkExperience = function () {
+  fetch(`${apiBaseUrl}${endpointExperiencia}`, {
+    method: "GET",
+    mode: "cors",
+    headers: new Headers({ "content-type": "application/json" }),
+  })
+    .then(handleErrors)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data["experiencia-laboral"]);
+      const workExperience = data["experiencia-laboral"];
+      let experienceItems = "";
+      let first = true;
+      for (const experience of workExperience) {
+        experienceItems += `<div class="${
+          first ? "" : "mt-4"
+        } text-gray-600 w-full mt-2 overflow-hidden">
+                <p class="text-black font-bold">${experience.puesto}</p>
+                <p>${experience.empresa}</p>
+                <p class="italic">${parseDate(
+                  experience.fechaInicio
+                ).getFullYear()}-${getExperienceYear(
+          parseDate(experience.fechaFin)
+        )}</p>
+                <p class="mt-2">${experience.descripcion}</p>
+                </div>`;
+        first = false;
+      }
+      const experienceItemsElem = document.getElementById("experience-items");
+      if (experienceItemsElem !== null)
+        experienceItemsElem.innerHTML = experienceItems;
+    })
+    .catch((error) => {
+      showError("Error al obtener experiencias");
+      console.error("Error:", error);
+    });
+};
+var getModalInputs = function (): NodeListOf<HTMLInputElement> {
+  return document.querySelectorAll("#modal-form");
+};
+var closeModal = function () {
+  if (!modal) return;
+  // Clean inputs
+  var nodeModalInputs: NodeListOf<any> = getModalInputs();
+  var modalInputs: any[] = nodeModalInputs[0];
+  for (var _i = 0, _a = modalInputs; _i < _a.length; _i++) {
+    var n = _a[_i];
+    n.value = "";
+  }
+  modal.classList.add("hidden");
+};
+var submitModal = function () {
+  // Read and store inputs
+  var contact: any = {};
+  var nodeModalInputs: NodeListOf<any> = getModalInputs();
+  var modalInputs: any[] = nodeModalInputs[0];
+  for (var _i = 0, _a = modalInputs; _i < _a.length; _i++) {
+    var n = _a[_i];
+    contact[n.id] = n.value;
+  }
+  console.log(contact);
+  postContact(contact);
+  closeModal();
+};
+var showModal = function () {
+  modal.classList.remove("hidden");
+};
+var showError = function (text: string) {
+  errorAlertMessage.innerHTML = text;
+  errorAlert.classList.remove("hidden");
+};
+var closeError = function () {
+  errorAlert.classList.add("hidden");
+};
+(function () {
+  modal =
+    document.querySelector("#contact-modal") ||
+    (function () {
+      throw "No modal";
+    })();
+
+  errorAlert =
+    document.querySelector("#error-alert") ||
+    (function () {
+      throw "No error alert";
+    })();
+
+  errorAlertMessage =
+    document.querySelector("#error-alert-message") ||
+    (function () {
+      throw "No error alert message";
+    })();
+
+  const modalSubmit = document.querySelector("#modal-submit");
+  if (modalSubmit != null) modalSubmit.addEventListener("click", submitModal);
+
+  const modalCancel = document.querySelector("#modal-cancel");
+  if (modalCancel != null) modalCancel.addEventListener("click", closeModal);
+
+  const contactMe = document.querySelector("#contact-me");
+  if (contactMe != null) contactMe.addEventListener("click", showModal);
+
+  const errorAlertClose = document.querySelector("#error-alert-close");
+  if (errorAlertClose != null)
+    errorAlertClose.addEventListener("click", closeError);
+
+  getWorkExperience();
+})();
